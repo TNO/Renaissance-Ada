4af9dab104675d40e18fa875ec5d45aa6406502d
pjljvandelaar <pierre.vandelaar@tno.nl>
2022-01-21 09:28:27 +0100

    Improved documentation / limitation + added context info to remove limitation in the future

--- src/libraries/Rejuvenation_Lib/src/rejuvenation-simple_factory.ads
+++ src/libraries/Rejuvenation_Lib/src/rejuvenation-simple_factory.ads
@@ -60,5 +60,36 @@ package Rejuvenation.Simple_Factory is
    --  Ada files in those subprojects will be include based on the
    --  value of the Recursive flag
    --  Pre: Project_Filename points to an Ada project file
+   --
+   --  Current implementation doesn't work around libadalang limitation:
+   --
+   --  LIBADALANG.PROJECT_PROVIDER.UNSUPPORTED_VIEW_ERROR:
+   --  selected project is aggregate and has more than one sub-project
+   --
+   --  TODO: Make a workable / reasonable solution!
+   --  suggestions / background info:
+   --
+   --  The situation is more nuanced: even though indeed a given Libadalang
+   --  context cannot see whole aggregate projects when they contain
+   --  "conflicting" units, it is perfectly possible for a single command to
+   --  create one context per aggregated project and run your analysis on all
+   --  of them. This specific case is illustrated in our documentation with
+   --  an example: see
+   --  <https://docs.adacore.com/live/wave/libadalang/html/libadalang_ug/examples/
+   --   aggregate_projects.html
+   --  #creating-one-provide-context-for-each-aggregated-project>.
+   --
+   --  Note that because of the semantics of aggregate projects, it would
+   --  make no sense for a single Libadalang context to see units from all
+   --  aggregated projects at once without adding much complexity to the
+   --  Libadalang API. For instance, given that multiple units may define the
+   --  same type several times, the P_Referenced_Decl property would need to
+   --  be modified to return not "THE declaration" referenced by some
+   --  expression, but all declarations it may refer to (depending on the
+   --  "view" inside the aggregated project). This would be probably a great
+   --  burden for both us (Libadalang implementers) and all Libadalang users
+   --  (need to deal with a much more complex API even when not dealing with
+   --  aggregate projects).
+
 
 end Rejuvenation.Simple_Factory;
