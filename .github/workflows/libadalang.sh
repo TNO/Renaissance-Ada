6c08231e60aaa3be7a9c612cc268fde4f1e3c9fd
pjljvandelaar <pierre.vandelaar@tno.nl>
2022-03-18 17:29:24 +0100

    Experiment with github actions + libadalang download & installation

---
+++ .github/workflows/libadalang.sh
@@ -0,0 +1,6 @@
+PREFIX=/tmp/ADALIB_DIR
+FILE=libadalang-linux-master-static.tar.gz
+mkdir -p $PREFIX
+aws s3 cp s3://adacore-gha-tray-eu-west-1/libadalang/$FILE . --sse=AES256
+tar xzf $FILE -C $PREFIX
+rm -f -v $FILE
