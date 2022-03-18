95991f11d292c7b47b548d86777e8d9653b6f8ca
pjljvandelaar <pierre.vandelaar@tno.nl>
2022-03-18 17:48:07 +0100

    Update libadalang.sh

--- .github/workflows/libadalang.sh
+++ .github/workflows/libadalang.sh
@@ -1,5 +1,5 @@
 PREFIX=/tmp/ADALIB_DIR
-FILE=libadalang-linux-master-static.tar.gz
+FILE=libadalang-linux-22.1-static.tar.gz
 mkdir -p $PREFIX
 aws s3 cp s3://adacore-gha-tray-eu-west-1/libadalang/$FILE . --sse=AES256
 tar xzf $FILE -C $PREFIX
