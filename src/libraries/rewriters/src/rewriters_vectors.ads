04bc80201cbb79e76fffb4ad95252f6efb854526
pjljvandelaar <pierre.vandelaar@tno.nl>
2022-06-08 09:39:36 +0200

    moved code to separate project

--- src/libraries/rewriters/src/rewriters_vectors.ads
+++
@@ -1,5 +0,0 @@
-with Ada.Containers.Indefinite_Vectors;
-with Rewriters; use Rewriters;
-
-package Rewriters_Vectors is new Ada.Containers.Indefinite_Vectors
-  (Positive, Rewriter'Class);
--- src/libraries/rewriters/tests/src/rewriters_suite.ads
+++
@@ -1,7 +0,0 @@
-with AUnit.Test_Suites; use AUnit.Test_Suites;
-
-package Rewriters_Suite is
-
-   function Suite return Access_Test_Suite;
-
-end Rewriters_Suite;
